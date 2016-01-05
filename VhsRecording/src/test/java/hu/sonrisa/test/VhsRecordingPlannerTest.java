91fd7e163d5fa10efc82528b88b7c25240f86131
joe <joe@sonrisa.hu>
2016-01-05 16:20:33 +0100

    Test case to challenge correctness.

--- VhsRecording/src/test/java/hu/sonrisa/test/VhsRecordingPlannerTest.java
+++ VhsRecording/src/test/java/hu/sonrisa/test/VhsRecordingPlannerTest.java
@@ -39,5 +39,36 @@ public class VhsRecordingPlannerTest
 		assertEquals("Law & Order",schedule.get(1));
 		assertEquals("The Fresh Prince of Bel-Air",schedule.get(2));
 	}
+        
+        /**
+         * Test case: Input contains a long and few short shows. The long 
+         * one covers all the others.
+         * 
+         * Expected result: All the short shows will be recorded.
+         * 
+         */
+	@Test
+	public void testRecordingPlannerALongAndManyShort() 
+	{
+                // arrange
+		String listOfShows = "1000 1800 Law & Order\n"
+				+ "1100 1115 ER\n"
+                                + "1200 1215 Ellen\n"
+				+ "1300 1315 Family Matters\n"
+                                + "1400 1415 The Fresh Prince of Bel-Air\n"
+				+ "1500 1515 Pokémon";
+		VhsRecordingPlanner planner = new VhsRecordingPlanner();
+                
+                // act
+		List<String> schedule = planner.planRecording(listOfShows);
+		
+                // assert
+                final String[] expectedResult = {"ER", "Ellen", "Family Matters", "The Fresh Prince of Bel-Air", "Pokémon"};
+                assertEquals(expectedResult.length, schedule.size());                
+                for(int i = 0; i<expectedResult.length; i++){
+                    assertEquals(expectedResult[i], schedule.get(i));
+                
+                }
+	}
 
 }
